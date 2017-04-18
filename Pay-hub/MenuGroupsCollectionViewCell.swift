d401362409faae89a56a2ebfcf4c520eb0ecf42c
RSTI E-Services <rstintl@Vinays-MacBook-Air.local>
2017-04-18 15:36:59 +0530

    Menugroupsadded

---
+++ Pay-hub/MenuGroupsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuGroupsCollectionViewCell.swift
+//  Pay-hub
+//
+//  Created by RSTI E-Services on 18/04/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class MenuGroupsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Pay-hub/MenuItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuItemCollectionViewCell.swift
+//  Pay-hub
+//
+//  Created by RSTI E-Services on 18/04/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemCollectionViewCell: UICollectionViewCell {
+    
+}
