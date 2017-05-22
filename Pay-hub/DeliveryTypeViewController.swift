ac6016928d86fba662de6a68309796a43a6a6b07
RSTI E-Services <vinaygupta.iitkgp@gmail.com>
2017-05-22 11:05:07 +0530

    contribution1

---
+++ Pay-hub/DeliveryTypeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeliveryTypeViewController.swift
+//  Pay-hub
+//
+//  Created by RSTI E-Services on 22/05/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
