ecf29892d7fb12499dc832e898888f3c6a3c4f21
RSTI E-Services <vinaygupta.iitkgp@gmail.com>
2017-05-25 12:49:33 +0530

    review order added

---
+++ Pay-hub/getAddressTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  getAddressTableViewCell.swift
+//  Pay-hub
+//
+//  Created by RSTI E-Services on 23/05/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class getAddressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
