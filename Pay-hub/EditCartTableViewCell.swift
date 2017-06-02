ac4ca7d35c36a6f59fb3fd599a476098e1420807
RSTI E-Services <vinaygupta.iitkgp@gmail.com>
2017-06-02 16:32:15 +0530

    edit order

---
+++ Pay-hub/EditCartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EditCartTableViewCell.swift
+//  Pay-hub
+//
+//  Created by RSTI E-Services on 02/06/17.
+//  Copyright © 2017 RSTI E-Services. All rights reserved.
+//
+
+import UIKit
+
+class EditCartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
